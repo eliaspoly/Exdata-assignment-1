296aad6b691e04b46e7935b7cf8c7f645f92c78a
eliaspoly <eliasp@utas.edu.au>
2016-07-11 16:39:20 +1000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,10 @@
-f <- file.path(getwd(),"household_power_consumption.txt")
-data <- read.table(f,sep = ";", colClasses = c("character", "character", rep("numeric",7)), na = "?",skip = 66637, nrows = 2880)
+file <- file.path(getwd(),"household_power_consumption.txt")
+data <- read.table(file,sep = ";", colClasses = c("character", "character", rep("numeric",7)), na = "?", skip = 1)
 head(data)
 str(data)
+sub <- data[data$V1 %in% c("1/2/2007","2/2/2007") ,]
 
 #dev.new()
 png("plot1.png", width=480, height=480)
-hist(data$V3, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+hist(sub$V3, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
 dev.off()
